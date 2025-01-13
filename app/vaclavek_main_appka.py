d3ac5c8ac33d9cf33b959c6468b3389faa210371
Pavel Vaclavek <pvaclavek@cmgp.local>
2025-01-13 15:23:00 +0100

    lekce 12

--- app/vaclavek_main_appka.py
+++ app/vaclavek_main_appka.py
@@ -5,11 +5,19 @@ from vaclavek_config_appka import *
 
 def cml2(hmotnost, af):
         bmr=hmotnost*24.2
-        cml=bmr*af
-        cml=round(cml,2)
+        cml=round(bmr*af,2)
         return cml
 
+def group(pohlavi):
+        if pohlavi =="muž":
+                return "ahoj chlape"
+        elif pohlavi =="žena":
+                return "ahoj ženo"
+        elif pohlavi =="jiné":
+                return "ahoj"
+        
 
+                
 # Main Window Variables
 todays_date_str = dt.date.today().strftime("%d-%m-%Y") #this is a string
 todays_date_obj = dt.date.today() #this is an object
--- app/vaclavek_main_window_appka.py
+++ app/vaclavek_main_window_appka.py
@@ -2,27 +2,43 @@
 from guizero import *
 from vaclavek_main_appka import *
 
+### GUI functions
 def cml1():
     hmotnost = float(txtbox_weight.value)
     af=float(txtbox_af.value)
     cml=cml2(hmotnost, af)
     return cml
 
-### GUI functions
 def my_first_gui_function():
     text_welcome.value = "Ahoj, uživateli!"
     cmltxt = cml1()
     text_cml.value = f"tvoje cml je {cmltxt}"
 
+def group2():
+    souhlas = checkbox.value
+    if souhlas ==True:
+        pohlavi=choice.value
+        pozdraveni.value = group(pohlavi)
+    else:
+        pozdraveni.value="není souhlas"
+
+
 
 ### GUI App
-app = App(title="My App", width=775, height=650)
+app = App(title="My App", width=775, height=650, layout="auto")
 
 ## Window 1
 window1 = Box(app, visible=True)
 
 # Welcome text
 text_welcome = Text(window1, text=(f"Hi, user!"))
+pozdraveni = Text(window1, text=(f""))
+
+#button group
+choice = ButtonGroup(window1, options=["muž", "žena", "jiné"], selected=" ", command=group2)
+
+#check button
+checkbox = CheckBox(window1, text="souhlasím s odesláním")
 
 # Input activity factor
 text_af = Text(
@@ -44,16 +60,17 @@ txtbox_weight = TextBox(window1)
 text_cml = Text(window1, text ="")
 
 # Display an image
-image_widget = Picture(
-    window1,
-    image="resources/images/calculating_cml.png",
-    width=680,
-    height=480,
-    align="bottom"
-)
+#image_widget = Picture(
+   # window1,
+   # image="resources/images/calculating_cml.png",
+   # width=680,
+   # height=480,
+   # align="bottom"
+#)
 
 #button
 button = PushButton(window1, command=my_first_gui_function)
+
 
 app.display()
 
